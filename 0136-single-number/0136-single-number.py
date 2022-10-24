324e9c6dc05e4dbf9ad7db7a1ec9cb68ae44082d
isaac <60953696+akuwuh@users.noreply.github.com>
2022-10-24 02:49:32 -0400

    Time: 150 ms (86.47%), Space: 16.6 MB (83.98%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,8 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = {}
         
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                seen[nums[i]] = 1 #adding value at index to seen and keeping track of how many has been seen
-            else:
-                seen[nums[i]] +=1
+        seen = dict(Counter(nums))
             
         for i in seen:
             if seen[i] != 2:
