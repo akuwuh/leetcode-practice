1128285ad6e6738499a62b23809445591f22d1e1
isaac <60953696+akuwuh@users.noreply.github.com>
2023-02-08 02:31:28 -0500

    Time: 140 ms (62.02%), Space: 15.1 MB (22.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,38 +1,37 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        nums.pop()
+        n = len(nums)
+        if n <= 1:
+            return n
         
-        def maxreach(sub: List[int]): #returns index that has the most reach
-            if len(sub) == 1:
-                return 0
-            maxi = 0
-            index = 0
-            for i,val in enumerate(sub):
-                if i + val >= maxi:
-                    maxi = i + val
-                    index = i
-            return index
+        if nums[0] > n-1:
+            return 1
         
-        if len(nums) <= 2:
-            return len(nums) - 1
-
-        if nums[0] == len(nums) - 2:
+        if nums[0] == n-1:
             return 2
 
-        i = 0
-        counter = 0
-        
-        while nums[i] + i < len(nums)- 1:
+        counter = 1
+        curr = nums[0] 
+        globalreach = 0 
 
-            if nums[i] >= len(nums) - 1:
+
+        for i,j in enumerate(nums):
+            val = j + i
+            if val >= n:
+                counter +=1
                 break
-            
-            lol = nums[i+1: i+ nums[i] + 1]
-            i += maxreach(lol) + 1
-            counter +=1
 
-            print(i)
-        
-        return counter + 1
+            if i < curr: 
+                globalreach = max(globalreach,val)
+                continue
+            counter+=1
+            curr = max(globalreach,val)
             
+        return counter
+  
+
+
+
             
             
