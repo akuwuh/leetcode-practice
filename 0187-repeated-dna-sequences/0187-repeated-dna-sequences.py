1a3a40c99601436059b10f7d230b1d0955fd864d
isaac <60953696+akuwuh@users.noreply.github.com>
2023-02-13 18:52:07 -0500

    Time: 50 ms (98.79%), Space: 27.7 MB (14.15%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -5,12 +5,12 @@ class Solution:
             return []
         # if longer than 10
         seen = defaultdict()
-        ans = []
+        ans = set()
         for i in range(10,n + 1):
             new = s[i-10:i]
 
-            if new in seen and new not in ans:
-                ans.append(new)
+            if new in seen:
+                ans.add(new)
                 continue
             
             seen[new] = 0
