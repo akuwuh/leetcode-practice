f8d303871c02881e68d436c6c6a0685b4385e218
isaac <60953696+akuwuh@users.noreply.github.com>
2023-02-21 15:53:27 -0500

    Time: 190 ms (31.44%), Space: 23.7 MB (86.81%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        if n == 1: return nums[0]
+
+        l, r = 0, n - 1
+
+        # cases:
+
+        # if m and m - 1 is the same val and r - m is even -> on left
+        # else if r - m is odd -> on right
+
+        # m and m + 1 is the same val and m - l is even -> on right
+        # else if m - l is odd -> on left 
+        
+        # on right assign l = m + 1
+        # on left assign r = m - 1
+
+        # 0 1 2  3  4  5  6 
+        #[7,7,10,11,11,12,12]
+
+        while r - l > 1:
+            m = (l+r)//2
+            if nums[m-1] != nums[m] and nums[m+1] != nums[m]: #case where m is single val   
+                return nums[m]
+
+            elif nums[m] == nums[m-1]:
+                if (r-m)%2 == 0:
+                    r = m
+                else:
+                    l = m + 1
+
+            else: # nums[m] == nums[m+1]
+                if (m-l)%2 == 1:
+                    r = m - 1
+                else:
+                    l = m
+        return nums[l] if nums[m] == nums[m+1] else nums[r]
+
+            
